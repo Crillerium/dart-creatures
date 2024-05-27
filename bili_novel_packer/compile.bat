8d46d9335f7da3e2deb95cb188754ce2f60823a3
crillerium <crillerium@outlook.com>
2024-05-27 12:09:59 +0800

    New Project

--- bili_novel_packer/compile.bat
+++ bili_novel_packer/compile.bat
@@ -1,5 +1,1 @@
-@echo off
-del build /q /s /f
-rd build /q /s
-mkdir build
 dart compile exe bin/main.dart -o ./build/bili_novel_packer-0.2.14.exe
---
+++ blogger/bin/blogger.dart
@@ -0,0 +1,5 @@
+import 'package:blogger/blogger.dart' as blogger;
+
+void main(List<String> arguments) {
+  print('Hello world: ${blogger.calculate()}!');
+}
